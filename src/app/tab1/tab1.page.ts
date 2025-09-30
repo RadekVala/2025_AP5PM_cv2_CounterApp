9f0fe0d2973b6208cc581ff8279ea0956af0e8d2
vala <vala@utb.cz>
2025-09-30 11:27:12 +0200

    Odstan stary nepotrebny kod pocitadla (nyni v komponente counter) pridej onSaved event handler

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput, IonItem } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { CounterComponent } from '../counter/counter.component';
 
 interface SavedCounter {
@@ -11,7 +11,7 @@ interface SavedCounter {
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent, CounterComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CounterComponent],
 })
 export class Tab1Page {
 
@@ -20,13 +20,7 @@ export class Tab1Page {
 
   constructor() {}
 
-  onIncrement() {
-    console.log('Increment');
-    this.count++
-  }
-
-  onReset() {
-    console.log('Reset');
-    this.count = 0
+  onSaved(event: { name: string, count: number}) {
+     console.log('Ulozeno: ', event.name, ',', event.count)
   }
 }
